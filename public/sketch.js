95d9d9adf9bf5c9eed12c457b14c67c1f2a3f7fe
Ryan Whitehead <ryebread9001@gmail.com>
2024-01-01 20:08:44 -0700

    init

---
+++ public/sketch.js
@@ -0,0 +1,230 @@
+const socket = io(`ws://localhost:5000`);
+
+let players = [];
+let clientPlayers = {};
+let plats = [];
+
+socket.on("connect", () => {
+	console.log("connected");
+})
+
+socket.on("map", (map) => {
+	plats = map;
+	for (var i = 0; i < plats.length; i++) {
+		let x = plats[i].x;
+		let y = plats[i].y;
+		let w = plats[i].w;
+		let h = plats[i].h;
+		let type = plats[i].type;
+		let color = plats[i].color;
+
+		plats[i] = new Platform(x, y, w, h, color, type);
+	}
+})
+
+socket.on("players", (serverPlayers) => {
+
+	for (var i = 0; i < serverPlayers.length; i++) {
+		if (!(serverPlayers[i].id in clientPlayers)) {
+			clientPlayers[serverPlayers[i].id] = new Player(500-(playerSize/2), 500-(playerSize/2), 0, 0, 'red', 'main')
+		} else {
+			clientPlayers[serverPlayers[i].id].updateFromServer(serverPlayers[i])
+		}
+	}
+})
+
+var canvas = document.getElementById("imgCanvas");
+var context = canvas.getContext("2d");
+canvas.width = 1000;
+canvas.height = 1000;
+let width = canvas.width;
+let height = canvas.height;
+//const colors = {'red', 'purple', 'green', 'blue', 'orange'};
+
+let player = document.getElementById('player'); // image refs
+let cliff = document.getElementById('cliff');
+let grass = document.getElementById('grass');
+let grass2 = document.getElementById('grass2');
+let grass3 = document.getElementById('grass3');
+let grass4 = document.getElementById('grass4');
+
+const controlMapper = {
+	'main' : { left: false, up: false, right: false, down: false },
+	'second' : { left: false, up: false, right: false, down: false }
+}
+// 65, 87, 68, 85
+const friction = 0.75;
+let tileNum = 25;
+let tileSize = width / tileNum;
+const playerSize = tileSize-1;
+const walkingSpeed = 0.7;
+
+const scale = 1;
+const pWidth = playerSize;
+const pHeight = playerSize;
+const scaledWidth = scale * pWidth;
+const scaledHeight = scale * pHeight;
+
+const cycleLoop = [0, 1, 0, 2];
+
+function Player(x, y, xSpeed, ySpeed, color, playerID) {
+
+	this.xSpeed = xSpeed;
+	this.ySpeed = ySpeed;
+	this.size = playerSize;
+	this.xCenter = x + (this.size/2);
+	this.yCenter = y + (this.size/2)
+  	this.color = color;
+	this.playerID = playerID;
+	this.canJump = true;
+	this.colFrame - false;
+	this.dir = 0;
+	this.cycle = 0;
+	this.frameCount = 0;
+
+	this.drawCharacter = function() {
+		context.fillStyle = this.color;
+		context.fillRect(this.xCenter-(this.size/2), this.yCenter-(this.size/2), this.size, this.size);
+	}
+
+	this.drawFrame = function() {
+		this.frameCount++;
+		
+		if (this.frameCount%7==0) {
+			if (Math.abs(this.xSpeed) > 0.5 || Math.abs(this.ySpeed) > 0.5) {
+				if (this.cycle == 3) {
+					this.cycle = 0;
+				} else {
+					this.cycle++;
+				}
+			} else {
+				this.cycle = 0;
+			}
+		}
+		
+
+		context.drawImage(player,
+		cycleLoop[this.cycle] * this.size/1.2, this.dir * this.size * 0.93, this.size*0.75, this.size*0.97,
+		this.xCenter-(this.size/2), this.yCenter-(this.size/2), this.size, this.size);
+	}
+
+	this.updateFromServer = function(serverPlayer) {
+		this.xCenter = serverPlayer.xCenter;
+		this.yCenter = serverPlayer.yCenter;
+		this.xSpeed = serverPlayer.xSpeed;
+		this.ySpeed = serverPlayer.ySpeed;
+		this.dir = serverPlayer.dir;
+	}
+
+  	this.update = function() {	
+		this.drawFrame();
+	}
+}
+
+function Platform(x, y, w, h, color, type) {
+	this.x = x;
+	this.y = y;
+	this.w = w;
+	this.h = h;
+	this.color = color;
+	this.type = type;
+
+	this.drawPlatform = function() {
+		//context.fillStyle = this.color;
+		//context.fillRect(this.x-(this.w/2), this.y-(this.h/2), this.w, this.h);
+		context.drawImage(cliff,
+			0, 0, this.w, this.h,
+			this.x-(this.w/2), this.y-(this.h/2), this.w*2.5, this.h*2);
+
+	}
+
+	this.update = function() {
+		if (this.type == 'trigger') {
+
+		} else {
+			this.drawPlatform();
+		}
+	}
+}
+
+function drawPlats(plats) {
+	for (var i = 0; i < plats.length; i++) {
+		plats[i].update();
+	}
+}
+
+function drawGrass() {
+	let randomGrass = [grass, grass2, grass3, grass4];
+	for (var i = 0; i < width/tileNum; i++) {
+		for (var j = 0; j < height/tileNum; j++) {
+			let random = getRandomInt(0, randomGrass.length-1);
+			if (i == 12 || j == 12) {
+				context.drawImage(randomGrass[3],
+					0, 0, 76, 76,
+					i*tileSize, j*tileSize, 76, 76)
+			} else {
+				context.drawImage(grass,
+					0, 0, 76, 76,
+					i*tileSize, j*tileSize, 76, 76);
+			}
+			
+		}
+	}
+	
+}
+
+function draw() {
+	context.clearRect(0, 0, width, height);
+	drawGrass();
+	if (plats.length > 0) drawPlats(plats);
+	if (Object.keys(clientPlayers).length > 0) {
+		for (const player in clientPlayers) {
+			clientPlayers[player].update();
+		}
+	}
+	
+}
+
+document.body.onload = function() {
+
+  function gameLoop() {
+    draw();
+    window.requestAnimationFrame(gameLoop);
+  }
+  window.requestAnimationFrame(gameLoop);
+};
+
+function getRandomInt(min,max) {
+    return Math.floor((Math.random()*max)+min);
+}
+
+function keyDownHandler(event) {
+	if (event.keyCode === 39) {
+		controlMapper.main.right = true;
+	} else if (event.keyCode === 37) {
+		controlMapper.main.left = true;
+	}
+	if (event.keyCode === 40) {
+		controlMapper.main.down = true;
+	} else if (event.keyCode === 38) {
+		controlMapper.main.up = true;
+	}
+	socket.emit('inputs', controlMapper.main);
+}
+
+function keyUpHandler(event) {
+	if (event.keyCode === 39) {
+		controlMapper.main.right = false;
+	} else if (event.keyCode === 37) {
+		controlMapper.main.left = false;
+	}
+	if (event.keyCode === 40) {
+		controlMapper.main.down = false;
+	} else if (event.keyCode === 38) {
+		controlMapper.main.up = false;
+	}
+	socket.emit('inputs', controlMapper.main);
+}
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
---
+++ src/index.js
@@ -0,0 +1,214 @@
+const express = require('express');
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, { /* options */ });
+
+const TICK_RATE = 30;
+
+let players = [];
+const inputsMap = {};
+
+let width = 1000;
+let height = 1000;
+
+const friction = 0.75;
+const walkingSpeed = 1.7;
+let tileNum = 25;
+let tileSize = width / tileNum;
+const playerSize = tileSize-1;
+
+const scale = 1;
+const pWidth = playerSize;
+const pHeight = playerSize;
+const scaledWidth = scale * pWidth;
+const scaledHeight = scale * pHeight;
+const platNum = 60;
+
+function Platform(x, y, w, h, color, type) {
+	this.x = x;
+	this.y = y;
+	this.w = w;
+	this.h = h;
+	this.color = color;
+	this.type = type;
+}
+
+function getRandomInt(min,max) {
+    return Math.floor((Math.random()*max)+min);
+}
+
+function compare( a, b ) {
+	if ( a.y < b.y ){
+		return -1;
+	}
+	if ( a.y > b.y ){
+		return 1;
+	}
+	return 0;
+}
+
+function platformRandom() {
+	let plats = [];
+	for (var i = 0; i < platNum; i++) {
+		plats[i] = new Platform(getRandomInt(1,tileNum)*tileSize-tileSize/2,
+		getRandomInt(1,tileNum)*tileSize-tileSize/2,
+		tileSize-1,
+		tileSize-1, 'white', 'plat');
+	}
+	plats.sort( compare );
+	return plats;
+}
+
+const map = platformRandom();
+
+function getDims(a, b) {
+	return {
+		playerLeft : a.xCenter - a.size/2,
+		playerRight : a.xCenter + a.size/2,
+		playerTop : a.yCenter - a.size/2,
+		playerBottom : a.yCenter + a.size/2,
+		platformLeft : b.x - b.w/2,
+		platformRight : b.x + b.w/2,
+		platformTop : b.y - b.h/2,
+		platformBottom : b.y + b.h/2
+	}
+}
+
+function isCollide(a, b){
+	let d = getDims(a, b);
+	return {
+		'col' : (d.playerBottom > d.platformTop &&
+		 d.playerTop < d.platformBottom &&
+		 d.playerLeft < d.platformRight &&
+		 d.playerRight > d.platformLeft),
+		'xCol' : (d.playerLeft < d.platformRight &&
+		d.playerRight > d.platformLeft),
+		'xColR' : (d.playerRight > d.platformLeft) && (d.playerRight < (d.platformRight - b.w/2)),
+		'xColL' : (d.playerLeft < d.platformRight) && (d.playerLeft > (d.platformLeft + b.w/2)),
+		'yCol' : (d.playerBottom > d.platformTop &&
+		 d.playerTop < d.platformBottom),
+		'yColB' : (d.playerBottom > d.platformTop) && (d.playerBottom < (d.platformBottom - b.h/2)),
+ 		'yColT' : (d.playerTop < d.platformBottom) && (d.playerTop > (d.platformTop + b.h/2)),
+	};
+}
+
+function checkCollisionForPlayer(player, plats) {
+
+    for (var i = 0; i < plats.length; i++) {
+        let col = isCollide(player, plats[i]);
+        if (col.col) {
+            let triggered = false;
+
+            let d = getDims(player, plats[i]);
+
+            if (col.yCol && col.yColB && !triggered) {
+                player.ySpeed = 0;
+                //player1.xSpeed = 0;
+                player.yCenter = d.platformTop - player.size/2 - 1;
+                //console.log('Bottom');
+                triggered = true;
+            }
+            if (col.xCol && col.xColL && !triggered) {
+                player.xSpeed = 0;
+                //player1.ySpeed = 0;
+                player.xCenter = d.platformRight + player.size/2 + 1;
+                //console.log('Left');
+                triggered = true;
+            }
+            if (col.xCol && col.xColR && !triggered) {
+                player.xSpeed = 0;
+                //player1.ySpeed = 0;
+                player.xCenter = d.platformLeft - player.size/2 - 1;
+                //console.log('Right');
+                triggered = true;
+            }
+            if (col.yCol && col.yColT && !triggered) {
+                player.ySpeed = 0;
+                //player1.xSpeed = 0;
+                player.yCenter = d.platformBottom + player.size/2 + 1;
+                //console.log('Top');
+                triggered = true;
+            }
+            console.log(triggered);
+        }
+    }
+}
+
+function tick() {
+    for (const player of players) { // main logic loop
+
+        if (inputsMap[player.id].left) {
+            player.xSpeed -= walkingSpeed;
+            player.dir = 2;
+        } else {
+            //player.xSpeed = 0;
+        }
+        if (inputsMap[player.id].right) {
+            player.xSpeed += walkingSpeed;
+            player.dir = 3;
+        } else {
+            //player.xSpeed = 0;
+        }
+        if (inputsMap[player.id].up) {
+            player.ySpeed -= walkingSpeed;
+            player.dir = 1;
+        } else {
+
+        }
+        if (inputsMap[player.id].down) {
+            player.ySpeed += walkingSpeed;
+            player.dir = 0;
+        } else {
+
+        }
+
+        player.xSpeed*=friction;
+        player.ySpeed*=friction;
+
+        player.xCenter+=player.xSpeed;
+        player.yCenter+=player.ySpeed;
+
+        checkCollisionForPlayer(player, map);
+    }
+    //console.log(players);
+    // return to client
+    io.emit("players", players);
+}
+
+io.on("connect", (socket) => {
+    console.log("user connected", socket.id);
+    players.push({
+        id: socket.id,
+        xCenter: 500-(playerSize/2),
+        yCenter: 500-(playerSize/2),
+        xSpeed: 0,
+        ySpeed: 0,
+        dir: 0,
+        size: playerSize
+    });
+    inputsMap[socket.id] = {
+        left: false,
+        up: false,
+        right: false,
+        down: false
+    }
+    socket.emit("map", map);
+
+    socket.on("inputs", (inputs) => {
+        // console.log(socket.id, inputs);
+        inputsMap[socket.id] = inputs;
+    })
+
+    socket.on("disconnect", () => {
+        players = players.filter((player) => player.id !== socket.id);
+    })
+})
+
+
+app.use(express.static("public"));
+
+httpServer.listen(5000);
+
+setInterval(tick, 1000/TICK_RATE);
