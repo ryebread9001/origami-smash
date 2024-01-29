42b6bf6d399e158f95b3c2e1fb397f016f4e173b
Ryan Whitehead <ryebread9001@gmail.com>
2024-01-29 01:10:13 -0700

    added arrows and death and score update

--- public/sketch.js
+++ public/sketch.js
@@ -1,9 +1,12 @@
 const socket = io('', {transports: ['websocket']});
 
 let players = [];
+let clientArrows = {};
 let clientPlayers = {};
 let plats = [];
+let sprites = [];
 let clientPlayerId = "Unknown Player";
+let canShoot = true;
 
 const messages = document.getElementById("messages");
 const chatInput = document.getElementById("chat-input");
@@ -70,17 +73,31 @@ socket.on("newChat", (chat) => {
 })
 
 socket.on("players", (serverPlayers) => {
-
 	for (var i = 0; i < serverPlayers.length; i++) {
 		if (!(serverPlayers[i].id in clientPlayers)) {
 			clientPlayers[serverPlayers[i].id] = new Player(500-(playerSize/2), 500-(playerSize/2), 0, 0, 'red', serverPlayers[i].id)
 		} else {
-			clientPlayers[serverPlayers[i].id].updateFromServer(serverPlayers[i])
+			clientPlayers[serverPlayers[i].id].updateFromServer(serverPlayers[i]);
+		}
+
+		if (serverPlayers[i].id == socket.id) {
+			score.innerText = serverPlayers[i].score;
 		}
 	}
 	let numActive = Object.keys(clientPlayers).length;
 	activePlayers.innerText = numActive;
-	
+})
+
+socket.on("arrows", (serverArrows) => {
+	serverArrows.forEach((arw) => {
+		if (!(arw.id in clientArrows)) {
+			clientArrows[arw.id] = new Arrow(arw.x, arw.y, arw.w, arw.h, arw.dir, arw.id);
+		} else if (arw.dead) {
+			delete clientArrows[arw.id];
+		} else {
+			clientArrows[arw.id].updateFromServer(arw);
+		}
+	})
 })
 
 var canvas = document.getElementById("imgCanvas");
@@ -96,6 +113,11 @@ let grass = document.getElementById('grass');
 let grass2 = document.getElementById('grass2');
 let grass3 = document.getElementById('grass3');
 let grass4 = document.getElementById('grass4');
+let arrowR = document.getElementById('arrowR');
+let arrowL = document.getElementById('arrowL');
+let arrowU = document.getElementById('arrowU');
+let arrowD = document.getElementById('arrowD');
+let skull = document.getElementById('skull');
 const activePlayers = document.getElementById('active-players');
 const score = document.getElementById('score');
 const usernameInput = document.getElementById("username-input");
@@ -124,6 +146,19 @@ const scaledHeight = scale * pHeight;
 
 const cycleLoop = [0, 1, 0, 2];
 
+function Sprite(x, y, w, h, img) {
+	this.x = x;
+	this.y = y;
+	this.w = w;
+	this.h = h;
+	this.img = img;
+	this.drawSprite = function() {
+		context.drawImage(img,
+			0, 0, this.w, this.h,
+			this.x-(this.w/2), this.y-(this.h/2), this.w, this.h);
+	}
+}
+
 function Player(x, y, xSpeed, ySpeed, color, playerID) {
 
 	this.xSpeed = xSpeed;
@@ -139,6 +174,9 @@ function Player(x, y, xSpeed, ySpeed, color, playerID) {
 	this.cycle = 0;
 	this.frameCount = 0;
 	this.name = "";
+	this.dead = false;
+	this.previousXCenter;
+	this.previousYCenter;
 
 	this.drawCharacter = function() {
 		context.fillStyle = this.color;
@@ -173,17 +211,75 @@ function Player(x, y, xSpeed, ySpeed, color, playerID) {
 	}
 
 	this.updateFromServer = function(serverPlayer) {
+		this.previousXCenter = this.xCenter;
+		this.previousYCenter = this.yCenter;
 		this.xCenter = serverPlayer.xCenter;
 		this.yCenter = serverPlayer.yCenter;
 		this.xSpeed = serverPlayer.xSpeed;
 		this.ySpeed = serverPlayer.ySpeed;
 		this.dir = serverPlayer.dir;
+		this.score = serverPlayer.score;
+		this.dead = serverPlayer.dead;
 		if (serverPlayer.name) this.name = serverPlayer.name;
 	}
 
   	this.update = function() {	
 		this.drawFrame();
 		this.drawName();
+	}
+}
+
+function Arrow(x, y, w, h, dir) {
+	this.x = x;
+	this.y = y;
+	this.dir = dir;
+	this.w = w;
+	this.h = h;
+
+	if (this.dir == 0) {
+		this.drawImage = ()=>context.drawImage(arrowD,
+			0, 0, this.w, this.h,
+			this.x-(this.w/2), this.y-(this.h/2), this.w*2.5, this.h*2);
+	} else if (this.dir == 1) {
+		this.drawImage = ()=>context.drawImage(arrowU,
+			0, 0, this.w, this.h,
+			this.x-(this.w/2), this.y-(this.h/2), this.w*2.5, this.h*2);
+	} else if (this.dir == 2) {
+		this.drawImage = ()=>context.drawImage(arrowL,
+			0, 0, this.w, this.h,
+			this.x-(this.w/2), this.y-(this.h/2), this.w*2.5, this.h*2);
+	} else if (this.dir == 3) {
+		this.drawImage = ()=>context.drawImage(arrowR,
+			0, 0, this.w, this.h,
+			this.x-(this.w/2), this.y-(this.h/2), this.w*2.5, this.h*2);
+	}
+
+	this.updateFromServer = function(serverArrow) {
+		this.x = serverArrow.x;
+		this.y = serverArrow.y;
+	}
+
+	this.draw = function() {
+		// context.fillStyle = 'red';
+		// context.fillRect(this.x-(this.w/2), this.y-(this.h/2), this.w, this.h);
+		// if (this.dir == 0) {
+		// 	context.drawImage(arrowD,
+		// 		0, 0, this.w, this.h,
+		// 		this.x-(this.w/2), this.y-(this.h/2), this.w*2.5, this.h*2);
+		// } else if (this.dir == 1) {
+		// 	context.drawImage(arrowU,
+		// 		0, 0, this.w, this.h,
+		// 		this.x-(this.w/2), this.y-(this.h/2), this.w*2.5, this.h*2);
+		// } else if (this.dir == 2) {
+		// 	context.drawImage(arrowL,
+		// 		0, 0, this.w, this.h,
+		// 		this.x-(this.w/2), this.y-(this.h/2), this.w*2.5, this.h*2);
+		// } else if (this.dir == 3) {
+		// 	context.drawImage(arrowR,
+		// 		0, 0, this.w, this.h,
+		// 		this.x-(this.w/2), this.y-(this.h/2), this.w*2.5, this.h*2);
+		// }
+		this.drawImage()
 	}
 }
 
@@ -204,7 +300,7 @@ function Platform(x, y, w, h, color, type) {
 
 	}
 
-	this.update = function() {
+	this.draw = function() {
 		if (this.type == 'trigger') {
 
 		} else {
@@ -216,6 +312,12 @@ function Platform(x, y, w, h, color, type) {
 function drawPlats(plats) {
 	for (var i = 0; i < plats.length; i++) {
 		plats[i].update();
+	}
+}
+
+function drawIterable(iter) {
+	for (var i = 0; i < iter.length; i++) {
+		iter[i].draw();
 	}
 }
 
@@ -237,15 +339,39 @@ function drawGrass() {
 	}
 }
 
+function distance2D(x1, y1, x2, y2) {
+	let a = x1-x2;
+	let b = y1-y2;
+	return Math.floor(Math.sqrt(a*a + b*b))
+}
+
 function draw() {
 	context.clearRect(0, 0, width, height);
 	drawGrass();
-	if (plats.length > 0) drawPlats(plats);
-	if (Object.keys(clientPlayers).length > 0) {
-		for (const player in clientPlayers) {
-			clientPlayers[player].update();
+	if (sprites.length > 5) {
+		sprites = sprites.slice(1, sprites.length-1)
+	}
+	sprites.forEach((sprt)=>{
+		sprt.drawSprite();
+	})
+	if (plats.length > 0) drawIterable(plats);
+	if (Object.keys(clientArrows).length > 0) {
+		for (const arw in clientArrows) {
+			clientArrows[arw].draw();
 		}
 	}
+	if (Object.keys(clientPlayers).length > 0) {
+		for (const player in clientPlayers) {
+			let curr = clientPlayers[player]
+			curr.update();
+			if (distance2D(curr.xCenter, curr.yCenter, curr.previousXCenter, curr.previousYCenter) > 10) {
+				sprites.push(new Sprite(curr.previousXCenter, curr.previousYCenter, 39, 39, skull));
+				//curr.dead = false;
+			}
+
+		}
+	}
+	
 }
 
 document.body.onload = function() {
@@ -260,10 +386,27 @@ function getRandomInt(min,max) {
     return Math.floor((Math.random()*max)+min);
 }
 
+function keyPressedHandler(event) {
+	//if (event.repeat) return;
+	if (chatInput != document.activeElement && usernameInput != document.activeElement && canShoot) {
+		console.log("keyPressed")
+		if (event.keyCode === 32) {
+			console.log("shoot")
+			controlMapper.main.shoot = true;
+			canShoot = false;
+			socket.emit('inputs', controlMapper.main);
+			controlMapper.main.shoot = false;
+			document.onkeypress = null;
+		}
+	}
+}
+
 function keyDownHandler(event) {
+	let inputsToSend = [39, 68, 37, 65, 40, 83, 38, 87];
 	if (chatInput != document.activeElement && usernameInput != document.activeElement) {
 		if (event.keyCode === 39 || event.keyCode === 68) {
 			controlMapper.main.right = true;
+			
 		} else if (event.keyCode === 37 || event.keyCode === 65) {
 			controlMapper.main.left = true;
 		}
@@ -272,11 +415,16 @@ function keyDownHandler(event) {
 		} else if (event.keyCode === 38 || event.keyCode === 87) {
 			controlMapper.main.up = true;
 		}
-		socket.emit('inputs', controlMapper.main);
+		
+		if (inputsToSend.includes(event.keyCode)) {
+			controlMapper.main.shoot = false;
+			socket.emit('inputs', controlMapper.main);
+		}
 	}
 }
 
 function keyUpHandler(event) {
+	let inputsToSend = [32, 39, 68, 37, 65, 40, 83, 38, 87];
 	if (chatInput != document.activeElement && usernameInput != document.activeElement) {
 		if (event.keyCode === 39 || event.keyCode === 68) {
 			controlMapper.main.right = false;
@@ -288,9 +436,18 @@ function keyUpHandler(event) {
 		} else if (event.keyCode === 38 || event.keyCode === 87) {
 			controlMapper.main.up = false;
 		}
-		socket.emit('inputs', controlMapper.main);
+		if (event.keyCode === 32) {
+			canShoot = true;
+			controlMapper.main.shoot = false;
+			document.onkeypress = keyPressedHandler;
+		}
+		if (inputsToSend.includes(event.keyCode)) {
+			controlMapper.main.shoot = false;
+			socket.emit('inputs', controlMapper.main);
+		}
 	}
 }
 
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
+document.addEventListener("keypress", keyPressedHandler, false);
